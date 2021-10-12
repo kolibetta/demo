5642c156a32cd7f2e1305db1a68bd9ec2c9bb2f5
arun <savakarun@gmail.com>
2021-10-13 02:57:12 +0530

    DEMO - First commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:11
+EXPOSE 8080
+ADD target/demo.jar demo.jar
+ENTRYPOINT ["java", "-jar", "/demo.jar"]
+
---
+++ src/main/java/com/example/demo/resource/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class HomeController {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "welcome to zapcom";
+	}
+}
